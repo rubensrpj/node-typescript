c2fca128a4ed385f49bd05be45157b046d0dea4e
Rubens Pinheiro <rubens@atiz.com.br>
2022-03-18 11:15:31 -0300

    Add User Interface and post/get user

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express, { response } from 'express';
 import bodyparse from 'body-parser'
 
 export default class App {
-    private express : express.Application;
-
+    private express: express.Application;
+    private user: { id: number, name: string, email: string, phone: string }[] = [];
     constructor() {
         this.express = express();
         this.express.use(bodyparse.json());
@@ -12,10 +12,26 @@ export default class App {
 
     private routes() {
         this.express.post('/sum', (req, res) => {
-            const data = req.body
-            const result = data.value1 + data.value2
+            const { value1, value2 } = req.body
+            const result = value1 + value2
             res.send(JSON.stringify(result))
         });
+        this.express.post('/user', (req, res) => {
+            const { name, email, phone } = req.body
+            const user = {
+                id: this.user.length + 1,
+                name: name,
+                email: email,
+                phone: phone
+            }
+            this.user.push(user)
+            res.send(JSON.stringify(user))
+        });
+        this.express.get('/user/', (req, res) => {
+            const { id } = req.query
+            const user = this.user.find(user => user.id === (id === undefined ? 0 : +id))
+            res.send(JSON.stringify(user))
+        })
     }
 
     public start() {
