28cbb79aa631d9994649f2fe61108e9d87c86dea
Rubens Pinheiro <rubens@atiz.com.br>
2022-03-18 10:23:18 -0300

    Version with typescript full

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import express, { response } from 'express';
 import bodyparse from 'body-parser'
 
-const app = express();
-app.use(bodyparse.json());
-app.post('/sum', (req, res) => {
-    const data = req.body
-    const result = data.value1 + data.value2
-    res.send(JSON.stringify(result))
-});
-app.listen(4003);
+export default class App {
+    private express : express.Application;
+
+    constructor() {
+        this.express = express();
+        this.express.use(bodyparse.json());
+        this.routes();
+    }
+
+    private routes() {
+        this.express.post('/sum', (req, res) => {
+            const data = req.body
+            const result = data.value1 + data.value2
+            res.send(JSON.stringify(result))
+        });
+    }
+
+    public start() {
+        this.express.listen(4003)
+    }
+}
+
+const app = new App();
+app.start();
